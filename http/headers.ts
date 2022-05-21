d2cbd031e8eee975cce411c9ed73763d4f6da69b
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-05-21 15:33:43 +0200

    fix

--- http/headers.ts
+++ http/headers.ts
@@ -4,11 +4,3 @@ export const jsonHeaders = {
     'Accept': json,
     'Content-Type': json
 }
-
-// export const getAuthHeaders = (token: string) => {
-//     return {
-//         'Accept': json,
-//         'Content-Type': json,
-//         'Authorization': `Bearer ${token}`
-//     }
-// }
