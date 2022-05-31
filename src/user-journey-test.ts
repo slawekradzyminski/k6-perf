51aa98401b9f80d25d0946e0533a6c3f2f35ad8b
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-05-31 12:33:15 +0200

    report

--- src/user-journey-test.ts
+++ src/user-journey-test.ts
@@ -1,4 +1,4 @@
-import { sleep } from 'k6';
+import { JSONObject, sleep } from 'k6';
 import { Options } from 'k6/options';
 /* @ts-ignore */
 import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.1.0/index.js';
@@ -7,26 +7,43 @@ import { registerUser } from '../requests/register';
 import { loginUser } from '../requests/login';
 import { getUsers } from '../requests/users';
 import { editUser } from '../requests/edit';
+/* @ts-ignore */
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
-export let options:Options = {
-  vus: 3,
-  iterations: 3
+export const options: Options = {
+  stages: [
+    { duration: '2m', target: 10 },
+    { duration: '4m', target: 20 }, 
+    { duration: '30s', target: 0 }, 
+  ],
+  thresholds: {
+    http_req_failed: ['rate<0.1'],
+    http_req_duration: ['p(50)<1500'], 
+    checks: ['rate>0.9'],
+  },
 };
 
 export default () => {
   const user = randomUser()
   // 1
   registerUser(user)
-  sleep(randomIntBetween(1,3))
+  sleep(5)
 
   // 2
   const token = loginUser(user)
-  sleep(randomIntBetween(1,3))
+  sleep(5)
 
   // 3
   getUsers(token)
-  sleep(randomIntBetween(1,3))
+  sleep(2)
 
   // 4
   editUser(user, token)
+  sleep(5)
 };
+
+export function handleSummary(data: JSONObject) {
+  return {
+    "summary.html": htmlReport(data),
+  };
+}
