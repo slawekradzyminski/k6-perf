7215257131f509444e55c5c1ca14bf8c8f4be9c7
slawekradzyminski <slawomir.radzyminski@gmail.com>
2022-06-03 15:24:19 +0200

    june37

--- src/user-journey.ts
+++ src/user-journey.ts
@@ -1,4 +1,4 @@
-import { JSONObject, sleep } from 'k6';
+import { check, JSONObject, sleep } from 'k6';
 import { Options } from 'k6/options';
 import { getRandomUser } from '../util/user';
 import { register } from '../request/registerRequest';
@@ -13,22 +13,66 @@ import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 import { refreshToken } from '../request/refreshTokenRequest';
 import { updateUser } from '../request/updateUserRequest';
 import { repeat, runWithProbability } from '../util/requestUtil';
+import { SharedArray } from 'k6/data';
+import http from 'k6/http';
+import { baseUrl } from '../config/constants';
+import { LoginRequest } from '../domain/loginTypes';
+import { jsonHeaders } from '../http/headers';
+import { getRandomIndex } from '../util/random';
 
 export const options: Options = {
-  stages: [
-    { duration: '2m', target: 10 },
-    { duration: '30s', target: 60 }, 
-    { duration: '2m', target: 5 }, 
-    { duration: '30s', target: 60 }, 
-    { duration: '2m', target: 0 }
-  ],
+  scenarios: {
+    userJourney: {
+      executor: 'ramping-vus',
+      exec: 'userJourney',
+      startVUs: 0,
+      stages: [
+        { duration: '1m', target: 2 },
+        { duration: '1m', target: 5 },
+      ],
+      gracefulRampDown: '60s',
+    },
+    loginJourney: {
+      executor: 'ramping-vus',
+      exec: 'loginJourney',
+      startVUs: 0,
+      stages: [
+        { duration: '1m', target: 2 },
+        { duration: '1m', target: 5 },
+      ],
+      gracefulRampDown: '60s',
+    },
+  },
   thresholds: {
     http_req_failed: ['rate<0.01'],
     checks: ['rate>0.9'],
   }
 };
 
-export default () => {
+const data = new SharedArray('users json data', () => {
+  return JSON.parse(open('./users.json')).users;
+});
+
+const loginRequestBody = (): string => {
+  const randomUser = data[getRandomIndex(data.length)] as LoginRequest;
+
+  const body: LoginRequest = {
+    username: randomUser.username,
+    password: randomUser.password
+  }
+  return JSON.stringify(body)
+}
+
+export function loginJourney() {
+  const loginResponse = http.post(`${baseUrl}/users/signin`, loginRequestBody(), {
+    headers: jsonHeaders
+  });
+  check(loginResponse, {
+    'status is 200': () => loginResponse.status === 200,
+  });
+};
+
+export function userJourney() {
   let token: string
   const user = getRandomUser()
 
@@ -48,8 +92,9 @@ export default () => {
 };
 
 export function handleSummary(data: JSONObject) {
+  const reportPath = `summary${Date.now()}.html`;
   return {
-    'summary.html': htmlReport(data),
+    [reportPath]: htmlReport(data),
     'stdout': textSummary(data, { indent: ' ', enableColors: true })
   };
 }
