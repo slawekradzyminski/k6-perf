65b78ae00f5ed4254b0e68b862de3b06293d305d
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-09-08 07:59:56 +0200

    report

--- src/user-journey.ts
+++ src/user-journey.ts
@@ -4,13 +4,14 @@ import { getRandomUser } from '../utils/user';
 import { register } from '../requests/register';
 import { login } from '../requests/login';
 import { checkGetAllUsers } from '../requests/getAllUsers';
+// @ts-ignore
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+// @ts-ignore
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 
 export let options: Options = {
     vus: 2,
-    iterations: 2,
-    thresholds: {
-        checks: ['rate>0.9'],
-    }
+    iterations: 2
 };
 
 export default () => {
@@ -22,5 +23,24 @@ export default () => {
     sleep(3)
     const token = login(user)
     sleep(1)
-    checkGetAllUsers(token)
+    executeNTimes(() => checkGetAllUsers(token), 4)
 };
+
+export function handleSummary(data: any) {
+    return {
+      "result.html": htmlReport(data),
+      stdout: textSummary(data, { indent: " ", enableColors: true }),
+    };
+  }
+
+function executeNTimes(fn: Function, n: number) {
+    for (let i = 0; i < n; i++) {
+        fn();
+    }
+}
+
+function executeWithProbability(fn: Function, probability: number) {
+    if (Math.random() < probability) {
+        fn();
+    }
+}
