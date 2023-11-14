37e42b5d71fa247d862b47c84a2f21b37031e5bd
slawekradzyminski <slawomir.radzyminski@gmail.com>
2023-11-14 12:09:03 +0100

    added comment

--- src/journey-test.ts
+++ src/journey-test.ts
@@ -34,6 +34,7 @@ const repeatNTimes = (fn: Function, n: number) => {
     for (let i = 0; i < Math.floor(n); i++ ) {
         fn()
     }
+    // last iteration with certain probability. So for 3.5 last iteration with probability 50%
     executeWithProbability(fn, n - Math.floor(n))
 }
 
