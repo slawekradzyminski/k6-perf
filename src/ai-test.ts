964715f4ec1264ac2fbb124d38ba88bc1f2293bb
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-11-06 13:54:32 +0100

    sea04

--- src/ai-test.ts
+++ src/ai-test.ts
@@ -1,11 +1,19 @@
 import { Options } from "k6/options";
+import { SharedArray } from "k6/data";
 import { login } from "../http/login";
+import { LoginRequest } from "../domain/loginTypes";
 
-export let options: Options = {
+const users = new SharedArray("users", () => {
+  return JSON.parse(open("./users.json")).users;
+});
+
+export const options: Options = {
   vus: 1,
-  iterations: 1
+  iterations: 1,
 };
 
 export default () => {
-  login("admin", "admin");
+  const user = users[Math.floor(Math.random() * users.length)] as LoginRequest;
+
+  login(user.username, user.password);
 };
