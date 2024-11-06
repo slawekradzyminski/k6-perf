59dd4edea163df58d44e33d766093fa368c7f020
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-11-06 13:36:24 +0100

    sea03

--- http/login.ts
+++ http/login.ts
@@ -3,11 +3,11 @@ import http from "k6/http";
 import { BASE_URL } from "../config/constants";
 import { params } from "../config/httpConfig";
 
-export const login = () => {
+export const login = (username: string, password: string) => {
   const url = `${BASE_URL}/users/signin`;
   const body = JSON.stringify({
-    username: "admin",
-    password: "admin",
+    username,
+    password,
   });
 
   const res = http.post(url, body, params);
--- src/ai-test.ts
+++ src/ai-test.ts
@@ -1,13 +1,11 @@
 import { Options } from "k6/options";
 import { login } from "../http/login";
 
-// konfiguracja testu wydajnościowego
 export let options: Options = {
   vus: 1,
   iterations: 1
 };
 
-// test funkcjonalny (scenariusz klienta pokrywający najwaniejsze endpointy)
 export default () => {
-  login();
+  login("admin", "admin");
 };
