990a2c7f51d907dd06c8f81c4df351dcf99b4e94
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-11-06 16:11:23 +0100

    sea11

--- src/ai-test.ts
+++ src/ai-test.ts
@@ -5,6 +5,8 @@ import { getRandomUser } from "../generator/userGenerator";
 import { register } from "../http/register";
 import { getUsers } from "../http/getUsers";
 import { editUser } from "../http/editUser";
+// @ts-ignore
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 
 export const options: Options = {
   vus: 1,
@@ -16,13 +18,29 @@ export const options: Options = {
   },
 };
 
+export function handleSummary(data: any) {
+  return {
+    "summary.html": htmlReport(data),
+  };
+}
+
+const repeat = (times: number, fn: () => void) => {
+  for (let i = 0; i < times; i++) {
+    fn();
+  }
+};
+
 export default () => {
   const user = getRandomUser();
-  register(user);
+  register(user); // 1x
   sleep(3);
-  const token = login(user.username, user.password);
-  sleep(1);
-  getUsers(token as string);
+  const token = login(user.username, user.password); // 1x
+
+  repeat(2, () => {
+    sleep(1);
+    getUsers(token as string);
+  });
+
   sleep(3);
-  editUser(user.username, token as string);
+  editUser(user.username, token as string); // 0.5
 };
