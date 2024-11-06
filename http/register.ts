0b81861e1a3c215fe04031d81a8fd9861a4ec914
slawekradzyminski <slawomir.radzyminski@gmail.com>
2024-11-06 14:43:42 +0100

    sea06

---
+++ http/register.ts
@@ -0,0 +1,16 @@
+import { check } from "k6";
+import http from "k6/http";
+import { BASE_URL } from "../config/constants";
+import { params } from "../config/httpConfig";
+import { User } from "../domain/registerTypes";
+
+export const register = (user: User) => {
+  const url = `${BASE_URL}/users/signup`;
+  const body = JSON.stringify(user);
+
+  const res = http.post(url, body, params);
+
+  check(res, {
+    "status is 201": (r) => r.status === 201
+  });
+};
--- src/ai-test.ts
+++ src/ai-test.ts
@@ -2,6 +2,7 @@ import { Options } from "k6/options";
 import { login } from "../http/login";
 import { sleep } from "k6";
 import { getRandomUser } from "../generator/userGenerator";
+import { register } from "../http/register";
 
 export const options: Options = {
   vus: 1,
@@ -10,8 +11,7 @@ export const options: Options = {
 
 export default () => {
   const user = getRandomUser();
-//   register(user);
-
+  register(user);
+  sleep(3);
   login(user.username, user.password);
-  sleep(1);
 };
