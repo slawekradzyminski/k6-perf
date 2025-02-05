cb504ec463d7b4d967a0cc047c054d8c3cbc40ce
slawekradzyminski <slawomir.radzyminski@gmail.com>
2025-02-05 11:25:08 +0100

    json test data

--- src/shopperjourney.ts
+++ src/shopperjourney.ts
@@ -1,16 +1,23 @@
 import { sleep } from 'k6';
 import { Options } from 'k6/options';
 import { login } from '../http/postSignIn';
+import { SharedArray } from 'k6/data';
+import { LoginRequest } from '../types/loginTypes';
+
+const data = new SharedArray('users', function () {
+  return JSON.parse(open('./users.json')).users;
+});
 
 export let options:Options = {
-  vus: 1,
-  iterations: 1,
+  vus: 5,
+  iterations: 5,
   thresholds: {
     checks: ['rate == 1'],
   },
 };
 
 export default () => {
-  login('admin', 'admin')
+  const user = data[Math.floor(Math.random() * data.length)] as LoginRequest;
+  login(user.username, user.password)
   sleep(1);
 };
